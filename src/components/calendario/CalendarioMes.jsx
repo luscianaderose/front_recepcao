604e85ad47adef5041d18fcb8c04f47a40ae28a8
luscianaderose <luscianaderose@gmail.com>
2024-09-10 10:13:05 -0300

    calendario

--- src/components/calendario/CalendarioMes.jsx
+++ src/components/calendario/CalendarioMes.jsx
@@ -3,35 +3,63 @@ import styles from "./CalendarioMes.module.css"
 function CalendarioMes(props){
     // console.log("dias do mes", props.mes["dias"])  NAO APAGAR ESSA NOTA
 
-    return(
+    const { dias, mes_display } = props.mes;
+
+    // Filtrar apenas os dias que são feriados
+    const feriadosDoMes = dias.filter(dia => dia.feriado);
+
+//     return(
+//         <div className={styles.card}>
+//             <div className={styles.cardTitle}>{props.mes["mes_display"]}</div>
+//             <div className={styles.cardBody}>
+//                 {/* {props.mes["dias"].map((dia, index) => (
+//                     <div key={index} className={styles.dia}>
+//                         <p>Dia do Mês: {dia.dia_mes}</p>
+//                         <p>Dia da Semana: {dia.dia_semana_display}</p>
+//                         {dia.feriado && <p>Feriado: {dia.feriado}</p>}
+//                     </div> */}
+
+//                 {props.mes["dias"].map((dia, index) => (
+//                     <div key={index} className={styles.dia}>
+//                         <p>{dia.dia_mes}</p>
+//                         <p>{dia.dia_semana_display}</p>
+//                         {dia.feriado && <p>Feriado: {dia.feriado}</p>}
+//                     </div>
+//                 ))}
+//             </div>
+//         </div>
+//     )
+// }
+
+    return (
         <div className={styles.card}>
-            <div className={styles.cardTitle}>{props.mes["mes_display"]}</div>
+            <div className={styles.cardTitle}>{mes_display}</div>
             <div className={styles.cardBody}>
-                {/* {props.mes["dias"].map((dia, index) => (
+                {dias.map((dia, index) => (
                     <div key={index} className={styles.dia}>
                         <p>Dia do Mês: {dia.dia_mes}</p>
                         <p>Dia da Semana: {dia.dia_semana_display}</p>
-                        {dia.feriado && <p>Feriado: {dia.feriado}</p>}
-                    </div> */}
-
-                {props.mes["dias"].map((dia, index) => (
-                    <div key={index} className={styles.dia}>
-                        <p>{dia.dia_mes}</p>
-                        <p>{dia.dia_semana_display}</p>
-                        {dia.feriado && <p>Feriado: {dia.feriado}</p>}
                     </div>
-
-
-
-
-
-
-
-
                 ))}
             </div>
+
+            {/* Exibir Feriados Abaixo dos Dias */}
+            {feriadosDoMes.length > 0 && (
+                <div className={styles.feriados}>
+                    <h4>Feriados</h4>
+                    {feriadosDoMes.map((feriado, index) => (
+                        <div key={index} className={styles.feriado}>
+                            <p>Dia: {feriado.dia_mes}</p>
+                            <p>Feriado: {feriado.feriado}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
-    )
-}
+    );
+    }
+
+
+
 
 export default CalendarioMes
