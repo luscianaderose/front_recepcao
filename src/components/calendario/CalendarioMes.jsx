f8ffa06f0e859b3150352b3607c7c8a7750531a0
luscianaderose <luscianaderose@gmail.com>
2024-09-12 20:01:12 -0300

    terminei calendaeio, comecei banco de dados

--- src/components/calendario/CalendarioMes.jsx
+++ src/components/calendario/CalendarioMes.jsx
@@ -4,9 +4,22 @@ function CalendarioMes(props){
     // console.log("dias do mes", props.mes["dias"])  NAO APAGAR ESSA NOTA
 
     const { dias, mes_display } = props.mes;
+    const primeiroDia = dias[0]
+
+    // console.log("primeiro dia ", primeiroDia, "mes ", mes_display)
+    const diasEmBranco = []
+
+    // console.log("primeiroDia ", primeiroDia)
+
+    for (var diaSemana = 0; diaSemana < primeiroDia["dia_semana"]; diaSemana++){
+        diasEmBranco.push(null)
+        // console.log("diaDaSemana", diaSemana, "primeiroDia[dia_semana] ", primeiroDia["dia_semana"], "mes_display", mes_display)
+    }
 
     // Filtrar apenas os dias que são feriados
     const feriadosDoMes = dias.filter(dia => dia.feriado);
+
+
 
     // // Calcular o primeiro e o último dia do mês
     // const firstDayOfMonth = new Date(props.ano, props.mesIndex - 1, 1); // props.mesIndex é o índice do mês (1 para Janeiro, 2 para Fevereiro, etc.)
@@ -34,8 +47,20 @@ function CalendarioMes(props){
                     </div>
                 ))}
             </div>
-
+            
             <div className={styles.cardBody}>
+            
+                {/* Exibir dias em branco */}
+                {diasEmBranco.map((dia) => (
+                    <div className={styles.dia}>
+                        <p>
+                            {dia}
+                        </p>
+                    </div>
+                    // console.log("dia ", dia)
+                ))}
+
+                {/* Exibir dias do mês*/}
                 {dias.map((dia, index) => (
                     <div key={index} className={styles.dia}>
                         <p className={dia.feriado ? styles.diaMesFeriado : ''}>
